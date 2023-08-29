4a2fad4ecb1f3e1b9a841cb38009c49a809c0f3f
ashley <calcium@pop-os.localdomain>
2023-08-28 20:32:10 -0700

    calciumsite update old

--- calciumsite/script.js
+++ calciumsite/script.js
@@ -1,10 +1,8 @@
 
 function start(){
 	var MAX_HEADERS = 2;
-	console.log("FUCK");
-	console.log(document.getElementById("img_header"));
-	document.getElementById("img_header").src = "assets/headers/"+Math.round(Math.random()*MAX_HEADERS)+".png";
-	console.log(document.getElementById("img_header").src);
+	//document.getElementById("img_header").src = "assets/headers/"+Math.round(Math.random()*MAX_HEADERS)+".png";
+	document.getElementById("img_header").src = "assets/headers/"+0+".png";
 }
 
 window.onload = start;
